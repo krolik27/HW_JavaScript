942b21d709111286453b22953686ee9b977085d8
krolik27 <33578226+krolik27@users.noreply.github.com>
2019-04-05 17:26:07 +0300

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,22 @@
+        function task1() {
+          var Tc = +prompt('Введите температуру в градусах по Цельсию: ');
+          var Tf = (9 / 5) * Tc + 32;
+          alert( Tc + ' градус Цельсия =' + Tf.toFixed(2) + ' градус Фаренгейта');
+        }
+
+        function task2() {
+          var admin;
+          var name;
+          name = prompt('Введите свое имя: ');
+          admin = name;
+          alert('Имя администратора: ' + admin);
+        }
+
+        function task3() {
+          var a = 1000;
+          var b = "108";
+          var z = a + b;
+          alert('Сумма a+b =' + z);
+        }
+
+        
